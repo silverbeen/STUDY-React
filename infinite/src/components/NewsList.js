ed4e95014687d6fcb31adc61316a9eca9b9bee03
강은빈 <kub0803@naver.com>
2021-01-09 22:49:50 +0900

    infiniteScroll 완성

--- infinite/src/components/NewsList.js
+++ infinite/src/components/NewsList.js
@@ -1,45 +1,29 @@
-import React, { useState, useEffect,useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import List from "./List";
 import * as S from "./style/List";
 
 const NewsList = () => {
-	const [articles, setArticles] = useState([]);
-	//API로 부터 받아온 다음 페이지 데ㅣ터 저장
-	const [articlesPaging, setArticlesPaging] = useState({next : undefined})
+  const [articles, setArticles] = useState([]);
+  //API로 부터 받아온 다음 페이지 데ㅣ터 저장
   const [loading, setLoading] = useState(false);
-	
-	//추가 데이터를 로드하는지 아닌지
-	const [fetching, setFetching] = useState(false);
-	
-	const fetchData = async () => {
-		setLoading(true);
-		
-		try {
-			const response = await axios.get(
-				"http://newsapi.org/v2/everything?q=bitcoin&from=2020-12-09&sortBy=publishedAt&apiKey=9e13930af2ed45e49e472714d8a895fc"
-			);
-			setArticles(response.data.data);
-			setArticlesPaging(response.data.paging);
-		} catch (e) {
-			console.error(e);
-		}
-		setLoading(false);
-	};
+  //추가 데이터를 로드하는지 아닌지
+  const [fetching, setFetching] = useState(false);
 
-	const fetchMoreData = async () => {
-		setFetching(true);
+  const fetchData = async () => {
+    setLoading(true);
 
-		await axios.get(articlesPaging.next)
-		.then((response) => {
-			const fetchData = response.data.data; // 피드 데이터 부분
-			//기존 데이터 배열과 새로 받아온 데이터 배열을 합쳐 새 배열을 만든다.
-			const mergedData = articles.concat(...fetchData);
-			setArticles(mergedData);
-		});
-		setFetching(false);
-	}
-
+    try {
+      const response = await axios.get(
+        "http://newsapi.org/v2/everything?q=bitcoin&from=2020-12-09&sortBy=publishedAt&apiKey=48c7bc1d5b954bd8b43a2005d7ac0037"
+      );
+      setArticles(response.data.articles);
+      console.log(response);
+    } catch (e) {
+      console.error(e);
+    }
+    setLoading(false);
+  };
 
   const infiniteScroll = useCallback(() => {
     let scrollHeight = Math.max(
@@ -53,17 +37,36 @@ const NewsList = () => {
     let clientHeight = document.documentElement.clientHeight;
 
     if (scrollTop + clientHeight === scrollHeight && fetching === false) {
-			fetchMoreData();
-		}
+      fetchMoreData();
+    }
   });
-
 
   useEffect(() => {
     fetchData();
-		window.addEventListener("scroll", infiniteScroll);
-		//스크롤 이벤트를 꼭 삭제 해줘야함
-    return () => window.removeEventListener("scroll", infiniteScroll);
-  }, [infiniteScroll]);
+
+    window.addEventListener("scroll", infiniteScroll, true);
+    //스크롤 이벤트를 꼭 삭제 해줘야함
+    return () => window.removeEventListener("scroll", infiniteScroll, true);
+  }, []);
+
+  const fetchMoreData = async () => {
+    setFetching(true);
+
+    try {
+      const response = await axios.get(
+        "http://newsapi.org/v2/everything?q=bitcoin&from=2020-12-09&sortBy=publishedAt&apiKey=48c7bc1d5b954bd8b43a2005d7ac0037"
+      );
+      console.log(response);
+      const fetchData = response.data.articles; //피드데이터 부분
+      //기존 데이터 배열과 새로 받아온 데이터 배열을 합쳐 새 배열을 만듬
+      // const mergedData = articles.concat(...fetchData);
+      setArticles((mergedData) => mergedData.concat(fetchData));
+    } catch (e) {
+      console.log(e);
+    }
+
+    setFetching(false);
+  };
 
   //로딩중
   if (loading) {
