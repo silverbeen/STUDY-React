1b285e16c2f0e590a20fc78ca7363ab6c2a550c7
강은빈 <kub0803@naver.com>
2021-11-25 08:52:18 +0900

    음성 인식 연습

---
+++ react-speech/src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import Speech from "./components/speech/Speech";
+
+function App() {
+  return <Speech />;
+}
+
+export default App;
---
+++ react-speech/src/components/speech/Speech.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { useSpeechRecognition } from "react-speech-kit";
+
+const Speech = () => {
+  const [value, setValue] = useState("");
+  const { listen, listening, stop } = useSpeechRecognition({
+    onResult: (result) => {
+      // 음성인식 결과가 value 상태값으로 할당됩니다.
+      setValue(result);
+    },
+  });
+
+  return (
+    <>
+      <div>
+        <div>{value}</div>
+        <button onMouseDown={listen} onMouseUp={stop}>
+          🎤
+        </button>
+        {listening && <div>음성인식 활성화 중</div>}
+      </div>
+    </>
+  );
+};
+
+export default Speech;
