d7c81ff53c8013f581e952a1eb4f5d08eda2bb84
강은빈 <kub0803@naver.com>
2022-01-14 16:02:12 +0900

    [MODIFY] hook distance

--- naver-map/src/lib/hook/distanceHook.ts
+++ naver-map/src/lib/hook/distanceHook.ts
@@ -1,5 +1,5 @@
 export function DistanceHook(distance: number) {
-  if (distance > 2000) {
+  if (distance > 1200) {
     return (distance / 1000).toFixed(1) + "km";
   } else {
     return Math.floor(distance) + "m";
