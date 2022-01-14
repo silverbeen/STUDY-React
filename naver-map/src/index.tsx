246efe29ab3c7f890ac6411d910ed60c6041b728
강은빈 <kub0803@naver.com>
2022-01-14 23:39:05 +0900

    [MODIFY]

--- naver-map/src/index.tsx
+++ naver-map/src/index.tsx
@@ -2,7 +2,6 @@ import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { RecoilRoot } from "recoil";
 import styled from "@emotion/styled";
 
@@ -23,8 +22,3 @@ ReactDOM.render(
   </RecoilRoot>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- naver-map/src/lib/api/testAPI.ts
+++ naver-map/src/lib/api/testAPI.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-
 export const test = async () => {
   try {
     const data = await axios(
